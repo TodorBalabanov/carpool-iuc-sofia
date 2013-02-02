d60012a1c7c7be43085ba56387c621ea690d0e05
todor <todor@asus-home-ubuntu>
2013-02-02 21:59:03 +0200

    Two stored procedures and new option for add car.

---
+++ frontend/add_car.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<html>
+<head>
+<?php include 'heading.php'; ?>
+  <title>.:: Carpool IUC Sofia - Add Car ::.</title>
+</head>
+
+<body>
+  <h1>Carpool IUC Sofia - Add Car</h1>
+
+<?php include 'navigation.php'; ?>
+
+  </p>
+
+  <form action="" method="post">
+	  <table border="0">
+		 <tr>
+		   <td align="right">Brand:</td>
+		   <td><input type="text" size="40" name="brand" id="brand"/></td>
+		 </tr>
+		 <tr>
+		   <td align="right">Model:</td>
+		   <td><input type="text" size="40" name="model" id="model"/></td>
+		 </tr>
+		 <tr>
+		   <td align="right">Registration:</td>
+		   <td><input type="text" size="40" name="registration" id="registration"/></td>
+		 </tr>
+		 <tr>
+		   <td align="right">Color:</td>
+		   <td><input type="text" size="40" name="color" id="color"/></td>
+		 </tr>
+	  </table>
+     <input type="submit" value="Add" name="add" id="add"/>
+</body>
+</html>
--- frontend/navigation.php
+++ frontend/navigation.php
@@ -4,6 +4,7 @@
       <td><a href="post_trip.php">Post Trip</a></td>
       <td><a href="search_trip.php">Search Trip</a></td>
       <td><a href="register.php">Register</a></td>
+      <td><a href="add_car.php">Add Car</a></td>
       <td><a href="login.php">Login</a></td>
       <td><a href="about.php">About</a></td>
       <td><a href="contacts.php">Contacts</a></td>
