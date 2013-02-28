4af61e4a87456c08f3e35a86b19abbe91cf8886f
todor <todor@black-asus-home>
2013-02-28 22:53:06 +0200

    GPL license was added.

--- frontend/about.php
+++ frontend/about.php
@@ -1,4 +1,26 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
+<?php
+/*******************************************************************************
+ *                                                                             *
+ * IUC Carpool is a web based carpool system.                                  *
+ * Project development is part of the education program at IUC-Sofia, Bulgaria.*
+ *                                                                             *
+ * Copyright (C) 2012-2013 by Todor Balabanov  ( tdb@tbsoft.eu )               *
+ *                                                                             *
+ * This program is free software: you can redistribute it and/or modify        *
+ * it under the terms of the GNU General Public License as published by        *
+ * the Free Software Foundation, either version 3 of the License, or           *
+ * (at your option) any later version.                                         *
+ *                                                                             *
+ * This program is distributed in the hope that it will be useful,             *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               *
+ * GNU General Public License for more details.                                *
+ *                                                                             *
+ * You should have received a copy of the GNU General Public License           *
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.        *
+ *                                                                             *
+ ******************************************************************************/
+?><!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN">
 <html>
 <head>
 <?php include 'heading.php'; ?>
@@ -13,6 +35,26 @@
   <p/>
 
 Internet based carpool system developed at International University College Sofia is part of education tutorials in few subjects as Multimedia, Databeses, Software Development and E-Business.
+
+<pre>
+IUC Carpool is a web based carpool system.
+Project development is part of the education program at IUC-Sofia, Bulgaria.
+
+Copyright (C) 2012-2013 by Todor Balabanov  ( tdb@tbsoft.eu )
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see &lt;http://www.gnu.org/licenses/&gt;.
+</pre>
 
 </body>
 </html>
